b17cf53bb881358ed4015db7bc030ae6c73a27bc
Matt Keef <mkeef1@gmail.com>
2014-08-27 12:51:53 -0500

    working

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -30,7 +30,9 @@ module.exports = function(app, express){
 
   app.use(security.bounce);
   app.delete('/logout', users.logout);
-
+  app.get('/profile/edit', users.edit);
+  app.put('/profile', users.update);
+  app.get('/profile', users.show);
   console.log('Express: Routes Loaded');
 };
 
